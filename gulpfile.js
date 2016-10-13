8891549393cdc2c40d65b61a3f72ef0b2c72334e
Iraquitan Cordeiro Filho <iraquitanfilho@gmail.com>
2016-10-13 16:19:23 -0300

    feat: Add gulp tasks to handle css and js minify
    Add gulp task to prepare the vendor fonts and images. Fix the
    styles and scripts tasks to concat in the correct order of import.
    Also adds minify-all task to run all the previous tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -3,28 +3,59 @@ var concat = require("gulp-concat");
 var uglify = require("gulp-uglify");
 var cleanCSS = require('gulp-clean-css');
 
-gulp.task('styles', function () {
-    return gulp
-        .src([
-            './source/vendor/font-awesome/css/font-awesome.min.css',
-            './source/vendor/open-sans/styles.css',
-            './source/vendor/source-code-pro/styles.css',
-            './source/vendor/lightgallery/css/lightgallery.min.css',
-            './source/vendor/justified-gallery/justifiedGallery.min.css'
+gulp.task('minify-all', ['vendor-styles', 'vendor-fonts', 'vendor-images', 'scripts']);
+
+gulp.task('vendor-styles', function () {
+    gulp.src([
+        './source/vendor/font-awesome/css/font-awesome.min.css',
+        './source/vendor/open-sans/styles.css',
+        './source/vendor/source-code-pro/styles.css',
+        './source/vendor/lightgallery/css/lightgallery.min.css',
+        './source/vendor/justified-gallery/justifiedGallery.min.css'
         ])
         .pipe(concat('vendor-styles.min.css'))
         .pipe(cleanCSS())
-        .pipe(gulp.dest('./source/vendor-dest/'))
+        .pipe(gulp.dest('./source/vendor/dest/css/'))
 });
 
 gulp.task('scripts', function () {
     gulp.src([
-        './source/vendor/jquery/2.1.3/jquery.min.js',
         './source/vendor/classie/classie.js',
-        './source/vendor/lightgallery/js/*.min.js',
-        './source/vendor/justified-gallery/jquery.justifiedGallery.min.js'
+        './source/vendor/lightgallery/js/lightgallery.min.js',
+        './source/vendor/lightgallery/js/lg-thumbnail.min.js',
+        './source/vendor/lightgallery/js/lg-pager.min.js',
+        './source/vendor/lightgallery/js/lg-autoplay.min.js',
+        './source/vendor/lightgallery/js/lg-fullscreen.min.js',
+        './source/vendor/lightgallery/js/lg-zoom.min.js',
+        './source/vendor/lightgallery/js/lg-hash.min.js',
+        './source/vendor/lightgallery/js/lg-share.min.js',
+        './source/vendor/lightgallery/js/lg-video.min.js',
+        './source/vendor/justified-gallery/jquery.justifiedGallery.min.js',
+        './source/js/main.js',
+        './source/js/contact.js',
+        './source/js/form.js'
     ])
         .pipe(concat('vendor-scripts.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('./source/vendor-dest/'))
+        .pipe(gulp.dest('./source/vendor/dest/js/'))
+});
+
+gulp.task('vendor-fonts', function () {
+    gulp.src([
+        './source/vendor/font-awesome/fonts/*',
+        './source/vendor/lightgallery/fonts/*'
+    ])
+        .pipe(gulp.dest('./source/vendor/dest/fonts'));
+    gulp.src([
+        './source/vendor/open-sans/fonts/*',
+        './source/vendor/source-code-pro/fonts/*'
+    ])
+        .pipe(gulp.dest('./source/vendor/dest/css/fonts'));
+});
+
+gulp.task('vendor-images', function () {
+    gulp.src([
+        './source/vendor/lightgallery/img/*'
+    ])
+        .pipe(gulp.dest('./source/vendor/dest/img'));
 });
